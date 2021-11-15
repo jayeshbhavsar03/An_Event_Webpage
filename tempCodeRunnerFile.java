8d82f198326bbb14c2f0a9b49f8a2a13466cd18b
Jayesh Bhavsar <bhavsarjayesh9370@gmail.com>
2021-11-15 10:59:27 +0530

    This is my frist multi pages and full responsive web page.

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,14 @@
+public class Main.java {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("This is sum of 5 numbers: ");
+        int num1 = 1;
+        int num2 = 2;
+        int num3 = 3;
+        int num4 = 4;
+        int num5 = 5;
+        int sum = num1 + num2 + num3 + num4 + num5;
+        System.out.println(sum);
+    }
+}
